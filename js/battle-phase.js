3053582969f5f72ef44ebb764a4eb55d0816b328
Biscuit <rjfarruggia@csus.edu>
2021-09-04 22:29:46 -0700

    add battle phase sprites

---
+++ js/battle-phase.js
@@ -0,0 +1,9 @@
+let p1 = document.getElementById("pokemon1");
+let p2 = document.getElementById("pokemon2");
+let type1 = p2.document.getElementById("type1");
+let type2 = document.getElementById("type2");
+let dmgmultiplier = 1;
+
+/*if (p1.type === "fire" && p2.type == "green") {
+
+}*/
